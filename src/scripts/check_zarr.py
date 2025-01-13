e11a74a304f5fdd0b68a2d897c90345e32ef2dcf
Matt <m.b.smith-5@umcutrecht.nl>
2025-01-13 13:47:06 +0100

    The check-zarr reads zarr image using py-zarr only.

--- src/scripts/check_zarr.py
+++ src/scripts/check_zarr.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python 
 
 
-import ome_zarr, zarr, ome_zarr.io, ome_zarr.writer, ome_zarr.reader
+#import ome_zarr, zarr, ome_zarr.io, ome_zarr.writer, ome_zarr.reader
+import zarr
+
 import numpy
 import sys
 
@@ -10,11 +12,43 @@ Opens a zarr file and prints things.
 
 Should use a common library command.
 """
-url = ome_zarr.io.parse_url(sys.argv[1])
-reader = ome_zarr.reader.Reader(url)
-img = list(reader())[0]
-print(img)
-dask_stack = img.data[0]
-print(dask_stack.shape)
-np_stack = numpy.array(dask_stack)
-print(numpy.sum(np_stack))
+
+def loadZarr( location ):
+
+    start = zarr.open_group(
+       store=sys.argv[1],
+       mode='r',
+    )
+    multiscales = start.attrs["multiscales"]
+    arrays = [ start[ key ] for key in start.array_keys() ]
+    return multiscales, arrays
+
+def printMetadata( metadata ):
+    axes = metadata["axes"]
+    for axis in axes:
+        unit = "NA"
+        if "unit" in axis:
+            unit = axis["unit"]
+        print("    > %s, %s, %s"%(axis["name"], axis["type"], unit) )
+    ds = metadata["datasets"]
+    for dsmd in ds:
+        for tf in dsmd["coordinateTransformations"]:
+            tp = tf["type"]
+            print( "  ", tp )
+            if tp in tf:
+                print( "    *", tf[ tp ] ) 
+
+print("Checking zarr file: ", sys.argv[1])
+metadata, array_data = loadZarr( sys.argv[1] )
+for md, arrays in zip(metadata, array_data):
+    print("Dataset: \"%s\""%arrays.name, arrays.shape )
+    printMetadata(md)
+print("finished checking!")
+#url = ome_zarr.io.parse_url(sys.argv[1])
+#reader = ome_zarr.reader.Reader(url)
+#img = list(reader())[0]
+#print(img)
+#dask_stack = img.data[0]
+#print(dask_stack.shape)
+#np_stack = numpy.array(dask_stack)
+#print(numpy.sum(np_stack))
