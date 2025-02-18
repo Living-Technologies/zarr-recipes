9ec8bc04bd2de731d3a2b578a6696b5e18a49386
Matt <m.b.smith-5@umcutrecht.nl>
2025-02-18 16:18:00 +0100

    Fixed calculation and removed superflous explaination.

--- src/scripts/multiscale_transform_check.py
+++ src/scripts/multiscale_transform_check.py
@@ -26,58 +26,23 @@ s0 is the original scale factor, it is often used to describe the size of a voxe
 The downsampling factor is the ration or original pixels to scaled pixels.
 
     f = ( D0/D1, H0/H1, W0/W1 )
-
-
  
-
 The volume will cover the same real volume, so the ratio of the scale factors will be 
 the downsampling used.
 
     s1 = f * s0
 
-To find the translation use the begining of the volume:
+To find the translation volume begins in the same pixel location for any resolution:
 
   pi = (-0.5, -0.5, -0.5) 
 
 Then we can solve.
 
- 0.5s0 + t0 = 0.5*s1 + t1
+ -0.5s0 + t0 = -0.5*s1 + t1
 
-  t1 = t0 + 0.5*(s0 - s1)
-  t1 = t0 + 0.5*(1 - f)s0
+  t1 = t0 + 0.5*(s1 - s0)
+  t1 = t0 + 0.5*(f - 1)s0
 
-## Transform in scaled pixels. **NOT USED**
-
-The coordnates represent the center of a pixel.
-The transform will transform a coordinate to "real space".
-
- r = ( p + t )*s
-
- p, pixel location. t is the translate portion of the transform. s is the scale.
-
-Each scale will have it's own set of pixels, transform, and scale. pi, ti, si respectivly.
-s0 is the original scale factor, it is often used to describe the size of a voxel.
-
-The downsampling factor is the ration or original pixels to scaled pixels.
-
-    f = ( D0/D1, H0/H1, W0/W1 )
-
-The volume will cover the same real volume, so the ratio of the scale factors will be 
-the downsampling used.
-
-    s1 = f * s0
-        
-To find the translation use the begining of the volume:
-
-  pi = (-0.5, -0.5, -0.5) 
-
-Then we can solve.
-
-  s0*( p0 + t0) = s1*(p1 + t1)
-  
-  0.5s0 + s0t0 = 0.5s1 + t1s1
-  t1s1 = 0.5*(s0 - s1) + s0t0
-  t1 = 0.5*(s0/s1 - 1) + t0 s0/s1
 """
 
 import ngff_zarr, numpy
@@ -103,7 +68,7 @@ ns = multiscales.images[1].scale
 nt = multiscales.images[1].translation 
 
 for key in scale_factors[0]:
-    t1 = ot[key] + 0.5*(1 - scale_factors[0][key])*os[key]
+    t1 = ot[key] + 0.5*(scale_factors[0][key] - 1)*os[key]
     print(t1, nt[key], ot[key])
     s1 = os[key]*scale_factors[0][key]
     print(key, nt[key] == t1, ns[key] == s1)
