c83e9f19acf19850994fa4486b5a881939970c87
Matt <m.b.smith-5@umcutrecht.nl>
2025-02-18 16:01:18 +0100

    Working on transform description for both pixel and scaled transform units.

--- src/scripts/multiscale_transform_check.py
+++ src/scripts/multiscale_transform_check.py
@@ -1,39 +1,83 @@
 #!/usr/bin/env python3
 
 """
+# Multiscale transformation checks.
+
 This is a test to make sure that the transforms are correct
 for multiscales.
 
-The idea is that the transform will transform a coordinate to "real space".
-Any coordinate will have the same "real space" value in each of the multiscales.
+There are two version 
+
+  r = ( p + t ) * s
+  r = p*s + t
+
+## Transform is in scaled units.
+
+The coordnates represent the center of a pixel.
+The transform will transform a coordinate to "real space".
+
+  r = s*p + t
+
+p, pixel location. t is the translate portion of the transform. s is the scale.
+
+Each scale will have it's own set of pixels, transform, and scale. pi, ti, si respectivly.
+s0 is the original scale factor, it is often used to describe the size of a voxel.
+
+The downsampling factor is the ration or original pixels to scaled pixels.
+
+    f = ( D0/D1, H0/H1, W0/W1 )
+
+
+ 
+
+The volume will cover the same real volume, so the ratio of the scale factors will be 
+the downsampling used.
+
+    s1 = f * s0
+
+To find the translation use the begining of the volume:
+
+  pi = (-0.5, -0.5, -0.5) 
+
+Then we can solve.
+
+ 0.5s0 + t0 = 0.5*s1 + t1
+
+  t1 = t0 + 0.5*(s0 - s1)
+  t1 = t0 + 0.5*(1 - f)s0
+
+## Transform in scaled pixels. **NOT USED**
+
+The coordnates represent the center of a pixel.
+The transform will transform a coordinate to "real space".
 
  r = ( p + t )*s
- 
+
  p, pixel location. t is the translate portion of the transform. s is the scale.
- 
- For a different multiscale there will be the same transforms available.
- 
- r = ( p' + t' )*s'
- 
- The p and p' represent pixel coordinates of the same volumes,  
- so when p = 0 then p' = 0. 
 
-    t's' = ts
- 
-The volume cannot change so p'=p/f.
+Each scale will have it's own set of pixels, transform, and scale. pi, ti, si respectivly.
+s0 is the original scale factor, it is often used to describe the size of a voxel.
 
-    (p' + t') = 0 when p + t = 0
-    
-    p/f + t' = 0 and combine to get t' = t/f
+The downsampling factor is the ration or original pixels to scaled pixels.
 
-There will be a t, s and a p', t', s' corresponding to the scale and the offset of the 
-scaled transform.
+    f = ( D0/D1, H0/H1, W0/W1 )
 
-    (p' + t')*s' = (p + t)*s
+The volume will cover the same real volume, so the ratio of the scale factors will be 
+the downsampling used.
 
-The transformation must work for multiple points so s` = s*f
+    s1 = f * s0
+        
+To find the translation use the begining of the volume:
 
-When p' = t', then p = t. 
+  pi = (-0.5, -0.5, -0.5) 
+
+Then we can solve.
+
+  s0*( p0 + t0) = s1*(p1 + t1)
+  
+  0.5s0 + s0t0 = 0.5s1 + t1s1
+  t1s1 = 0.5*(s0 - s1) + s0t0
+  t1 = 0.5*(s0/s1 - 1) + t0 s0/s1
 """
 
 import ngff_zarr, numpy
@@ -48,7 +92,6 @@ def createImage():
     img = ngff_zarr.ngff_image.NgffImage( arr, dims, scale=scale, translation=translate)
     return img
 
-
 image = createImage();
 scale_factors = [{'x':2, 'y':2, 'z':1}]
 multiscales = ngff_zarr.to_multiscales(image, scale_factors=scale_factors)
@@ -58,7 +101,9 @@ ot = multiscales.images[0].translation
 
 ns = multiscales.images[1].scale
 nt = multiscales.images[1].translation 
-print(os, ns)
 
-print( os["x"] == ns["x"]/2, os["y"] == ns["y"]/2, os["z"] == ns["z"] )
-print( ot["x"] == nt["x"]*2, ot["y"] == nt["y"]*2, os["z"] == ns["z"] )
+for key in scale_factors[0]:
+    t1 = ot[key] + 0.5*(1 - scale_factors[0][key])*os[key]
+    print(t1, nt[key], ot[key])
+    s1 = os[key]*scale_factors[0][key]
+    print(key, nt[key] == t1, ns[key] == s1)
