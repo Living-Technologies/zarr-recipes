7d048f21cb1bec688fd6d3c3b6ad930d96a094d5
Matthew Smith <msmith@n0098.manage.hpc>
2025-10-16 15:53:01 +0200

    Changed the output image type int8 was too small, and signed so fiji import ngff/zarr was truncating the value incorrectly.

--- src/scripts/predict_cellpose-2.py
+++ src/scripts/predict_cellpose-2.py
@@ -8,27 +8,31 @@ import dask
 import sys, pathlib
 
 if __name__=="__main__":
-    dask.config.set(workers=1)
+    dask.config.set(workers=2)
     inpth = pathlib.Path(sys.argv[1])
     opth = pathlib.Path( inpth.parent, inpth.name.replace(".zarr", "_cp-masks.zarr") )
 
     ms = ngff_zarr.from_ngff_zarr(inpth)
     img = ms.images[0]
 
-    indexes = [0, 1, 2]
+    indexes = [i for i in range(img.data.shape[0])]
 
     xy = img.scale["x"]
     z = img.scale["z"]
 
-    model = models.CellposeModel( gpu=False )
+    model = models.CellposeModel( gpu=True )
 
 
     def torchit( block_id, model=model, img=img, anisotropy=z/xy):
         y = model.eval(numpy.array(img.data[block_id[0],0]), z_axis=0, do_3D = True, anisotropy=anisotropy )
-        return y
+        pred = numpy.expand_dims(y[0], (0, 1))
+        print("shape after prediction: ", pred.shape)
+        return pred
 
     print("preparing")
-    out = dask.array.map_blocks( torchit, dtype="int8", chunks = ((1,)*len(indexes) ,1, *img.data.shape[2:]) )
+    chunks = ( (1, )*len(indexes), 1,  *img.data.shape[2:] )
+    print(chunks)
+    out = dask.array.map_blocks( torchit, dtype="uint16", chunks = chunks )
     print( "out stack: ", out.shape )
 
     oi = ngff_zarr.to_ngff_image(out, dims=img.dims, translation=img.translation, scale=img.scale)
